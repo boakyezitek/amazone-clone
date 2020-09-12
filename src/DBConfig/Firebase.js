27799eeaee653be02aebb441f1c32e089bef2c60
Boakye51 <53356674+Boakye51@users.noreply.github.com>
2020-09-12 15:42:26 +0000

    Clean app done by removing setupTests.js, App.test.js, and logo from src folder. Firebase.js file created to connect database from firebase store.

---
+++ src/DBConfig/Firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC3Tt9xKmoCM_RxfjcwyE565TdpyQdOsAk",
+    authDomain: "e-clone-9c4f5.firebaseapp.com",
+    databaseURL: "https://e-clone-9c4f5.firebaseio.com",
+    projectId: "e-clone-9c4f5",
+    storageBucket: "e-clone-9c4f5.appspot.com",
+    messagingSenderId: "787234991271",
+    appId: "1:787234991271:web:c3d7e4bf565d6a670e7f82"
+};
